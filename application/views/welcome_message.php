681a7ea6b222113f48863616cd9f00eedc84b2f7
Richard Robinson <kincade71@gmail.com>
2022-05-20 19:44:01 +0000

    Removed old content

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,78 +1,33 @@
- <div class="section visible-xs-block">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-2 col-sm-12 col-xs-12">
-		 <a href="#"> <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="MVXN78LHPJGLN">
-<!--input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"-->
-<!--img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"-->
+<!-- <div class="section visible-xs-block">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 col-sm-12 col-xs-12">
 
- <input type="submit" class="btn btn-lg btn-block" style="background-color: 6B377F; color:FFF" name="" value="DONATE">
-</form></a>
-
-
-          </div>
-		</div>
       </div>
 	</div>
-    <div class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <img src="../images/H4C.jpg" class="img-responsive col-md-10">
-          </div>
-          <div class="col-md-6">
-            <h1 style="color: 6B377F;">What is H4C?</h1>
-            <p style="font-size: 1.5em; line-height:2em">With H4C, we strive to empower students to learn technology in order to
-              positively impact their community. Whether it is exposing students to
-              the mobile app development world or helping businesses utilize technology to expand
-              their brand, we aim to help bring understanding of technoogy to those that
-              would not be exposed otherwise.&nbsp;</p>
-          </div>
-        </div>
+  </div>
+</div> -->
+<div class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        
+      </div>
+      <div class="col-md-6">
+
       </div>
     </div>
-    <div class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <h2 contenteditable="true" style="color: 6B377F;">Board Members</h2>
-            <ul>
-              <li>Richard Robinson, Co-Chair</li>
-              <li>Mari Robinson, Co-Chair
-                <br>
-              </li>
-              <li>Jennifer Davis</li>
-              <li>Lynmara Holloman</li>
-              <li>Nasia Lee</li>
-              <li>Amari Robinson-Honorary Student Member</li>
-            </ul>
-          </div>
-          <div class="col-md-6">
-            <h2 style="color: 6B377F;">We'd love to hear from you!</h2>
-            <p contenteditable="true">If you'd like to learn to more about H4C or if you'd like Hack for Community
-              to host an event for you, send us an email and we will get
-              back to you! <br/> <br/>Email: info@hackforcommunity.org</p>
-            <!-- <form role="form" action="welcome/contact" method="post"> 
-              <div class="form-group">
-                <label class="control-label" for="exampleInputEmail1" >Name</label>
-                <input class="form-control" id="exampleInputEmail1" placeholder="Enter name"
-                type="text" name="name">
-              </div>
-              <div class="form-group">
-                <label class="control-label" for="exampleInputPassword1" >Email</label>
-                <input class="form-control" id="exampleInputPassword1" name="email" placeholder="Enter email"
-                type="email">
-              </div>
-              <div class="form-group">
-                <label class="control-label">Message</label>
-                <textarea class="form-control" name="message"></textarea>
-              </div>
-              <div class="g-recaptcha pull-right" data-sitekey="6LfwPCkUAAAAAOsCRI15_3V7D7rzIo4bfLPaOBe7"></div>
-              <input  type="submit" class="btn btn-default btn-lg" style="background-color: 6B377F; color:FFF" value="Submit">
-            </form> -->
-          </div>
-        </div>
+  </div>
+</div>
+<div class="section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+
+      </div>
+      <div class="col-md-6">
+
       </div>
     </div>
+  </div>
+</div>
