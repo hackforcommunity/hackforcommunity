ae9eec2958bebbd6f7ee56b232e0dfebd6847a00
Richard Robinson <kincade71@gmail.com>
2022-05-23 15:45:26 -0400

    updated header and added background image

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,67 +7,67 @@
 	</div>
   </div>
 </div> -->
-<div class="section" style="height:500px; border: solid 1px black; background-image: url('images/DSC_5160.JPG'); background-position:center;">
-  <div class="container" style="padding-top:100px">
-    <div class="row" style="border: solid 1px red" >
-      <div class="col-md-4" style="border: solid 1px red; background-color: white; padding:50px 50px 50px 50px;">
+<div class="section section1">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
         <h1>HACK FOR COMMUNITY</h1>
         <p>If we can empower them with technology they can use it positively to change their communities.</p>
-        <a href="#" class="btn btn-primary outline">Our Mission</a>
+        <a href="#" class="btn btn-block btn-outline-primary pull-right">Our Mission</a>
       </div>
     </div>
   </div>
 </div>
 
-<div class="section" style="height:500px; border: solid 1px black; background-image: url('images/hack4community.jpg'); background-position:center;">
+<div class="section ">
   <div class="container">
-    <div class="row" style="border: solid 1px red">
-      <div class="col-md-12" style="border: solid 1px red">
+    <div class="row">
+      <div class="col-md-12">
       <h1>HACKATHON EVENTS</h1>
       <a href="#" class="btn btn-primary">LEARN MORE</a>
       </div>
-      <div class="col-md-6" style="border: solid 1px blue">
+      <div class="col-md-6">
 
       </div>
     </div>
   </div>
 </div>
 
-<div class="section" style="height:500px; border: solid 1px black; background-image: url('images/hack4community.jpg'); background-position:center;">
+<div class="section">
   <div class="container">
-    <div class="row" style="border: solid 1px red">
-      <div class="col-md-12" style="border: solid 1px red">
+    <div class="row">
+      <div class="col-md-12">
       <h1>TECHNOLOGY EDUCATION</h1>
       <a href="#" class="btn btn-primary">LEARN MORE</a>
       </div>
-      <div class="col-md-6" style="border: solid 1px blue">
+      <div class="col-md-6">
 
       </div>
     </div>
   </div>
 </div>
 
-<div class="section" style="border: solid 1px black">
+<div class="section">
   <div class="container">
-    <div class="row" style="border: solid 1px red">
-      <div class="col-md-12" style="border: solid 1px red">
+    <div class="row">
+      <div class="col-md-12">
       <h1>contact</h1>
       </div>
-      <div class="col-md-6" style="border: solid 1px blue">
+      <div class="col-md-6">
 
       </div>
     </div>
   </div>
 </div>
 
-<div class="section" style="height:500px; border: solid 1px black; background-image: url('images/hack4community.jpg'); background-position:center;">
+<div class="section">
   <div class="container">
-    <div class="row" style="border: solid 1px red">
-      <div class="col-md-12" style="border: solid 1px red">
+    <div class="row">
+      <div class="col-md-12">
       <h1>"It's not that we use, technology we live technology."</h1>
       <a href="#" class="btn btn-primary">MEET OUR BOARD</a>
       </div>
-      <div class="col-md-6" style="border: solid 1px blue">
+      <div class="col-md-6">
 
       </div>
     </div>
