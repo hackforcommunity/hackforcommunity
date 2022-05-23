9edcf8e41e12dacc94b700e02b532d1ed0e9d500
Richard Robinson <kincade71@gmail.com>
2022-05-23 08:42:41 -0400

    begin adding content

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,10 +10,12 @@
 <div class="section" style="border: solid 1px black">
   <div class="container">
     <div class="row" style="border: solid 1px red" >
-      <div class="col-md-12">
-        <h1>1</h1>
+      <div class="col-md-6" style="border: solid 1px red">
+        <h1>HACK FOR COMMUNITY</h1>
+        <p>If we can empower them with technology they can use it positively to change their communities.</p>
+        <a href="#" class="btn btn-primary outline">Our Mission</a>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="border: solid 1px blue">
 
       </div>
     </div>
@@ -23,10 +25,11 @@
 <div class="section" style="border: solid 1px black">
   <div class="container">
     <div class="row" style="border: solid 1px red">
-      <div class="col-md-12">
-      <h1>2</h1>
+      <div class="col-md-12" style="border: solid 1px red">
+      <h1>HACKATHON EVENTS</h1>
+      <a href="#" class="btn btn-primary">LEARN MORE</a>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="border: solid 1px blue">
 
       </div>
     </div>
@@ -36,10 +39,11 @@
 <div class="section" style="border: solid 1px black">
   <div class="container">
     <div class="row" style="border: solid 1px red">
-      <div class="col-md-12">
-      <h1>3</h1>
+      <div class="col-md-12" style="border: solid 1px red">
+      <h1>TECHNOLOGY EDUCATION</h1>
+      <a href="#" class="btn btn-primary">LEARN MORE</a>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="border: solid 1px blue">
 
       </div>
     </div>
@@ -49,10 +53,10 @@
 <div class="section" style="border: solid 1px black">
   <div class="container">
     <div class="row" style="border: solid 1px red">
-      <div class="col-md-12">
-      <h1>4</h1>
+      <div class="col-md-12" style="border: solid 1px red">
+      <h1>contact</h1>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="border: solid 1px blue">
 
       </div>
     </div>
@@ -62,10 +66,11 @@
 <div class="section" style="border: solid 1px black">
   <div class="container">
     <div class="row" style="border: solid 1px red">
-      <div class="col-md-12">
-      <h1>5</h1>
+      <div class="col-md-12" style="border: solid 1px red">
+      <h1>"It's not that we use, technology we live technology."</h1>
+      <a href="#" class="btn btn-primary">MEET OUR BOARD</a>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6" style="border: solid 1px blue">
 
       </div>
     </div>
