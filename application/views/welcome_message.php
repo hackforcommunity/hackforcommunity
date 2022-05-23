e7e2f427a1567ee6fbc3ccef133cd4f3f40c7cb3
Richard Robinson <kincade71@gmail.com>
2022-05-23 07:00:35 -0400

    Added html sections

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,11 +7,11 @@
 	</div>
   </div>
 </div> -->
-<div class="section">
+<div class="section" style="border: solid 1px black">
   <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        
+    <div class="row" style="border: solid 1px red" >
+      <div class="col-md-12">
+        <h1>1</h1>
       </div>
       <div class="col-md-6">
 
@@ -19,11 +19,90 @@
     </div>
   </div>
 </div>
-<div class="section">
+
+<div class="section" style="border: solid 1px black">
   <div class="container">
-    <div class="row">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>2</h1>
+      </div>
       <div class="col-md-6">
 
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>3</h1>
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>4</h1>
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>5</h1>
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>6</h1>
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>7</h1>
+      </div>
+      <div class="col-md-6">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section" style="border: solid 1px black">
+  <div class="container">
+    <div class="row" style="border: solid 1px red">
+      <div class="col-md-12">
+      <h1>8</h1>
       </div>
       <div class="col-md-6">
 
