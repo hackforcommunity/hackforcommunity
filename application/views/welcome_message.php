c343f5ceef0b49b897a9977d66f9a394b76d9f0d
Richard Robinson <kincade71@gmail.com>
2022-06-06 08:03:38 -0400

    added new section

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -49,25 +49,24 @@
   </div>
 </div>
 
-<div class="section">
+<div class="section contact">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
       <h1>contact</h1>
       </div>
-      <div class="col-md-6">
 
-      </div>
     </div>
   </div>
 </div>
 
-<div class="section">
+<div class="section section4">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-      <h1>"It's not that we use, technology we live technology."</h1>
-      <a href="#" class="btn btn-primary">MEET OUR BOARD</a>
+      <div class="col-md-6">
+      <!-- <h1>"It's not that we use, technology we live technology."</h1> -->
+      <blockquote>"It's not that we use, technology we live technology."</blockquote>
+      <a href="#" class="btn btn-block btn-outline-primary pull-right">MEET OUR BOARD</a>
       </div>
       <div class="col-md-6">
 
