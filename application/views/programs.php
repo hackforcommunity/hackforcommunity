4d70f7d1d3e3d9761d58502beb2741a6a9c45523
Richard Robinson <kincade71@gmail.com>
2022-06-08 07:05:46 -0400

    updated programs view.

--- application/views/programs.php
+++ application/views/programs.php
@@ -1,1 +1,70 @@
-Program
+<div class="section programs">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-12"">
+	  	<h1>Programs</h1>
+	  </div>
+	</div>
+  </div>
+</div>
+
+<div class="section programs1">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-12"">
+	  	To sign up for the current Pitch Hackathon, <a href="#" class="btn">click here!</a>
+	  </div>
+	</div>
+  </div>
+</div>
+
+<div class="section programs2">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-6"">
+	  	<img src="" />
+	  </div>
+	  <div class="col-md-6"">
+	  	<h2></h2>
+	  	<p></p>
+	 </div>
+	</div>
+  </div>
+</div>
+<div class="section programs3">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-6"">
+	  	<img src="" />
+	  </div>
+	  <div class="col-md-6"">
+	  	<h2></h2>
+	  	<p></p>
+	 </div>
+	</div>
+  </div>
+</div>
+
+<div class="section programs4">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-6"">
+	  	<img src="" />
+	  </div>
+	  <div class="col-md-6"">
+	  	<h2></h2>
+	  	<p></p>
+	 </div>
+	</div>
+  </div>
+</div>
+
+<div class="section programs5">
+  <div class="container">
+	<div class="row">
+	  <div class="col-md-12"">
+	  	<p></p>
+	  </div>
+	</div>
+  </div>
+</div>
