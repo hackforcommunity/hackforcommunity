7bcbe3d9b48621fd0b194096f8cad7271be0c7c8
Richard Robinson <kincade71@gmail.com>
2022-06-12 19:02:13 -0400

    finished progams view

--- application/views/programs.php
+++ application/views/programs.php
@@ -12,7 +12,7 @@
   <div class="container">
 	<div class="row">
 	  <div class="col-md-12"">
-	  	<h1>To sign up for the current Pitch Hackathon, <a href="#" class="btn btn-primary">click here!</a></h1>
+	  	<h1>To sign up for the current Pitch Hackathon, <a href="#" class="btn btn-link">click here!</a></h1>
 	  </div>
 	</div>
   </div>
@@ -21,13 +21,12 @@
 <div class="section programs2">
   <div class="container">
 	<div class="row">
-	  <div class="col-md-2"">
+	  <div class="col-md-4"">
 		  <img src="../images/PitchHackLogo.png" class="img-responsive"/>
-
 	  </div>
-	  <div class="col-md-10"">
+	  <div class="col-md-8"">
 	  	<h2>Pitch Hackathon</h2>
-	  	<p></p>
+	  	<p>A pitch Hackathon is a social event for students of all ages. During a Pitch Hackathon, attendees work individually or in teams to create a unique app idea to solve a problem for a community they are apart of. Once they have their idea, they design what it would look like and present it. These Hackathons can be conducted in person or online!</p>
 	 </div>
 	</div>
   </div>
@@ -35,14 +34,12 @@
 <div class="section programs3">
   <div class="container">
 	<div class="row">
-	  <div class="col-md-10"">
+	  <div class="col-md-8"">
 		  <h2>Coding Hackathon</h2>
-			<p></p>
+			<p>A coding hackathon is an in person event where students work with a team to come up with a unique idea or improve upon a specified idea in order to solve a problem. Then they design and code the product to present to the group and a winner is awarded.</p>
 	  </div>
-	  <div class="col-md-2"">
+	  <div class="col-md-4"">
 		  <img src="../images/CodingHackLogo.png" class="img-responsive"/>
-
-	  	
 	 </div>
 	</div>
   </div>
@@ -51,12 +48,12 @@
 <div class="section programs4">
   <div class="container">
 	<div class="row">
-	  <div class="col-md-2"">
+	  <div class="col-md-4"">
 	  	<img src="../images/TechEdLogo.png"  class="img-responsive"/>
 	  </div>
-	  <div class="col-md-10">
+	  <div class="col-md-8">
 	  	<h2>Tech Education</h2>
-	  	<p></p>
+	  	<p>Tech Education is the key to being able to thrive in the tech would! We provide one on one help as well as seminars and workshops for students around various technology topics.</p>
 	 </div>
 	</div>
   </div>
@@ -66,7 +63,7 @@
   <div class="container">
 	<div class="row">
 	  <div class="col-md-12"">
-	  	<p></p>
+	  	<h2>To lean more about how to bring one of our programs to your organization, send an email to info@hackforcommunity.org.</h2>
 	  </div>
 	</div>
   </div>
