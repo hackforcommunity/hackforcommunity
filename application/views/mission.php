5f2ead7356312294a9fd072cc7580367f6672ae7
Richard Robinson <kincade71@gmail.com>
2022-06-12 19:32:05 -0400

    started founders page

--- application/views/mission.php
+++ application/views/mission.php
@@ -2,7 +2,7 @@
   <div class="container">
 	<div class="row">
 	  <div class="col-md-12"">
-	  	<img src="../images/H4CLongLogo.png">
+	  	<img src="../images/H4CLongLogo.png" class="img-responsive">
 	  </div>
 	</div>
   </div>
