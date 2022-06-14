c410cbcf6a1b6f5f20ff886ee31f17637f9dca3d
Richard Robinson <kincade71@gmail.com>
2022-06-14 10:32:27 -0400

    finsihed fonders page

--- application/views/ourboard.php
+++ application/views/ourboard.php
@@ -11,9 +11,9 @@
   <div class="container">
 	<div class="row">
 	  <div class="col-md-3">
-		  <img src="../images/image4.png" class="img-responsive pull-left" />
-	  </div>
-	  <div class="col-md-8">
+		 <!-- <img src="../images/image4.png" class="img-responsive visible-xs" /> -->
+	  </div> 
+	  <div class="col-md-9">
 			<h1>Our Founders</h1>
 			<p>Hack for Community was founded in 2017 by Richard and Mari Robinson. Coming from engineering and education backgrounds, respectfully, they both realized the need for bringing technology education, experiences, and knowledge of careers in tech to underserved communities. They have three children that they love coding and designing with. Together they also own and operate WEBDMG LLC Technology Company.</p>
 		</div>
@@ -23,13 +23,13 @@
 <div class="section ourboard1">
   <div class="container">
 	<div class="row">
-	  <div class="col-md-8">
+	  <div class="col-md-7">
 		  <p>Richard Robinson is a global technology leader and software engineer. He has worked in the mobile application space of development for over 10 years developing and overseeing hundreds of mobile applications for small businesses and international corporations. He enjoys mentoring young engineers and teaching young people about the joys of tech.
 			  
 			  Mari Robinson is a Masters level educator. She was a classroom teacher for over 10 years before ending her tenure to homeschool her children in 2014. She is passionate about age appropriate learning and curriculum development, skills that she uses daily with her own children.</p>
 	  </div>
-	  <div class="col-md-4">
-			<img src="../images/image5.png"  class="img-responsive"/>
+	  <div class="col-md-5">
+			<!-- <img src="../images/image5.png"  class="img-responsive"/> -->
 		</div>
 	</div>
   </div>
