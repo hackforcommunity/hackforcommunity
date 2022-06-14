df7f0f892afaa803caaab7af5ecb3cb61611b384
Richard Robinson <kincade71@gmail.com>
2022-06-14 17:06:39 -0400

    added blog to nav

--- application/views/templates/main/header.php
+++ application/views/templates/main/header.php
@@ -56,6 +56,9 @@
               Our Founders
             </a>
           </li>
+          <li>
+            <a href="http://hackforcommunity.org/blog/">Blog</a>
+          </li>
           <form class="navbar-form navbar-right" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
             <input type="hidden" name="cmd" value="_s-xclick">
             <input type="hidden" name="hosted_button_id" value="MVXN78LHPJGLN">
